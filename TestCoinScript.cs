86c28e53aac3642f5e1493710116c0916a22ed99
Israr H <32361952+IsrarHussain99@users.noreply.github.com>
2019-07-28 01:44:52 +0100

    Add files via upload
    Add the asset folder to your Unity assets folder or alternatively Open the In-Game_ItemUnlockSystem_1.0.unitypackage and it will import. follow the instructions provided to setup your in-game store like (Fortnite)

---
+++ TestCoinScript.cs
@@ -0,0 +1,36 @@
+﻿//This is a test CoinScript for the example Scene. 
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestCoinScript : MonoBehaviour {
+
+	public int Coins;
+	public Text CoinsText;
+	public bool Reset = false;
+	// Change the PrefString to the Save Coins String, Or Leave it if you do not have one. 
+	void Start () {
+		//PlayerPrefs.SetInt ("CoinsPref", Coins);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		{
+		Coins = PlayerPrefs.GetInt ("CoinsPref");
+		CoinsText.text = "Coins: " + Coins.ToString ();
+	}
+		{
+			if (Reset){
+				Coins = 300;
+				PlayerPrefs.SetInt ("CoinsPref", Coins);
+			}
+		}
+	}
+
+	public void ResetButton ()
+	{
+		Reset = true;
+}
+	}
+
